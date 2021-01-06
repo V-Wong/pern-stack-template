910b8c7a5b5c1b47fa82cbbff82f5de5ce8b8d40
Vincent Wong <vincent@vwong.dev>
2021-01-06 21:37:52 +1100

    Add authentication interface to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,51 @@
 import React, { useState, useEffect } from 'react';
 
-import Feature from "./feature/Feature";
+import Feature from "./components/feature/Feature";
+import AuthContext from "./components/authContext/AuthContext";
 
 function App() {
+  const [user, setUser] = useState<any>({});
+  const [error, setError] = useState<string | null>(null);
   const [result, setResult] = useState("");
 
   useEffect(() => {
     (async () => {
       const response = await fetch("/api/project");
-      console.log(response);
       const result = await response.json();
 
       setResult(result[0].title);
     })();
   }, []);
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch("/api/auth/login/success", {
+          method: "GET",
+          credentials: "include",
+        });
+
+        if (response.status !== 200)
+          throw new Error("failed to authenticate user");
+
+        const json = await response.json();
+        setUser(json.user);
+      } catch (e) {
+        setError("Failed to authenticate user");
+      }
+    })();
+  }, []);
+
   return (
-    <Feature text={result} />
+    <AuthContext.Provider value={user}>
+      <Feature text={result} />
+      <div style={{ display: "flex", placeContent: "center" }}>
+        {user?.name ?
+          <p>User details: {user?.name}</p>
+          : <a href="http://localhost:5000/auth/github">Login</a>
+        }
+      </div>
+    </AuthContext.Provider>
   );
 }
 
---
+++ frontend/src/components/authContext/AuthContext.ts
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const AuthContext = createContext<any>(null);
+
+export default AuthContext;
